e30ae2bee6cd29746070c356323ada75beeb49c1
p0p3ye <40417611+p0p3yee@users.noreply.github.com>
2023-06-20 13:13:00 -0400

    Remove generate account in script

--- generate_keys.sh
+++ generate_keys.sh
@@ -10,15 +10,15 @@ die () {
 echo $1 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $1 provided"
 echo $2 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $2 provided"
 
-privateKeysArray=()
+# privateKeysArray=()
 
 for i in $(seq $1 $2 $END)
 do
   ssh-keygen -t rsa -b 2048 -m PEM -f "./keys/sk$i.pem" -N ""
   ssh-keygen -f "./keys/sk$i.pem" -e -m pem > "./keys/pk$i.pem"
   rm "./keys/sk$i.pem.pub"
-  pKey=$(openssl rand -hex 32)
-  privateKeysArray+=($pKey)
+  # pKey=$(openssl rand -hex 32)
+  # privateKeysArray+=($pKey)
 done
 
-sed -i .old -e "s/VALIDATOR_PRIVATE_KEYS=/VALIDATOR_PRIVATE_KEYS=$(IFS=, ; echo "${privateKeysArray[*]}")/g" .env
+# sed -i .old -e "s/VALIDATOR_PRIVATE_KEYS=/VALIDATOR_PRIVATE_KEYS=$(IFS=, ; echo "${privateKeysArray[*]}")/g" .env
