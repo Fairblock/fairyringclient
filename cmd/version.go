3f1ba7077d5a373d0d4cca09660cd0e091d0b2a6
p0p3yee <devp0p3yee@gmail.com>
2024-02-05 21:19:06 -0500

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+const ClientVersion = "v0.3.0"
+
+// configCmd represents the config command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version of FairyRingClient",
+	Long:  `Show version of FairyRingClient`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("FairyRingClient version: %s\n", ClientVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
